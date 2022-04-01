d68cb9c64ddc122bf61e0b6538bdff6ff8c1b84d
Kamyab <kamyab.zad@gmail.com>
2022-04-02 02:30:42 +0430

    Delete unused time.sh file

--- polybar_time_scripts/date.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-case $(redis-cli get polybar-date-toggle) in
-    1)
-        echo $(date +" %I:%M %p, %A, %d %B %Y, ") $(jdate +"%d %B %Y")
-        ;;
-    *)
-        echo $(date +" %I:%M %p")
-esac
